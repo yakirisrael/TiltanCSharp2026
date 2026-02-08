673dde4db3f233c53f7d6acebd72efd5478c470e
Your Name <yakir.israel1@gmail.com>
2026-02-08 19:10:34 +0200

    files and exceptions

--- Lecture07/obj/Debug/net10.0/Lecture07.AssemblyInfo.cs
+++ Lecture07/obj/Debug/net10.0/Lecture07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lecture07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40bed7cf4baf733842937341987a38ecf5e4c828")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f3ce3f700fdf191450e7cadebb8e19f04975a33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lecture07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lecture07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lecture11/Program.cs
@@ -0,0 +1,108 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+string path = Environment.CurrentDirectory + "../../../../SaveFiles/MyGame.sav";
+
+Item itm1 = new Item { Name = "Staff", Amount = 2 };
+Player p1 = new Player { Name = "Dekel", Hp = 67, Level = 30, item = itm1 };
+
+/*using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+{
+    using (BinaryWriter bw = new BinaryWriter(fs))
+    {
+        bw.Write(p1.item.Name);
+        bw.Write(p1.Name);
+        bw.Write(p1.Hp);
+        bw.Write(p1.Level);
+    }
+    
+}*/
+
+Player NewPlayer = new Player();
+NewPlayer.item = new Item();
+
+FileStream fs = null;
+
+try
+{
+        fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+        using (BinaryReader br = new BinaryReader(fs))
+        {
+            NewPlayer.item.Name = br.ReadString();
+            NewPlayer.Name = br.ReadString();
+            NewPlayer.Hp = br.ReadSingle();
+            NewPlayer.Level = br.ReadInt32();
+
+        }
+}
+catch (Exception e)
+{
+    Console.WriteLine("We are handling the crash here, message" + e.Message);
+
+}
+
+finally
+{
+    if (fs != null)
+    {
+        fs.Close();
+        Console.WriteLine("closing the file");
+    }
+    else
+    {
+        Console.WriteLine("file not exist");
+    }
+}
+
+
+/*
+
+
+
+string newContent = File.ReadAllText(path);
+Console.WriteLine(newContent);
+Player playerFromFile = JsonSerializer.Deserialize<Player>(newContent);
+Console.ReadKey();
+*/
+//string personSerialized = JsonSerializer.Serialize(p1);
+//Console.WriteLine(personSerialized);
+//File.WriteAllText(path, personSerialized);
+
+
+string content = "This is my game, copyrights resereved to Jon Battelfield";
+
+/*
+using (StreamReader reader = new StreamReader(path))
+{
+    bool matchStopSymnbol = false;
+    while (!matchStopSymnbol)
+    {
+        string line = reader.ReadLine();
+        Console.WriteLine(line);
+        matchStopSymnbol = line.Contains(",");
+    }
+}*/
+/*File.WriteAllText(path, content);
+File.AppendAllText(path, " appending Text");
+string readFromFile = File.ReadAllText(path);
+Console.WriteLine(readFromFile);
+*/
+//Console.WriteLine("Path: " + Environment.CurrentDirectory);
+
+
+
+class Item
+{
+    public string Name { get; set; }
+    public int Amount { get; set; }
+}
+
+class Player
+{
+    public string Name { get; set; }
+    public float Hp { get; set; }
+    public int Level { get; set; }
+    
+    public Item item { get; set; }
+}
